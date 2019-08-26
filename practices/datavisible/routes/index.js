2fbb9989c36eaa8fb51e8cb2c58bdd04943c5358
SueZ <565773372@qq.com>
2019-08-26 19:01:46 +0800

    day31

--- practices/datavisible/routes/index.js
+++ practices/datavisible/routes/index.js
@@ -1,9 +1,47 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const https = require("https");
+const cheerio = require("cheerio");
+
+function getMovieData() {
+  https.get(
+    "https://movie.douban.com/j/new_search_subjects?sort=U&range=0,10&tags=&start=0",
+    function(res) {
+      let data;
+      res.setEncoding("utf-8");
+      res.on("data", function(chunk) {
+        data = chunk;
+      });
+      res.on("end", function() {
+        let dataObj = JSON.parse(data);
+        let dataCon = dataObj.data;
+        dataCon.map(item => {
+          https.get(item.url, function(res) {
+            let itemdata;
+            res.setEncoding("utf-8");
+            res.on("data", function(chunk) {
+              itemdata += chunk;
+            });
+            res.on("end", function() {
+              let $ = cheerio.load(itemdata);
+              console.log(
+                $("#info .pl")
+                  .eq(4)
+                  .text()
+              );
+            });
+          });
+        });
+      });
+    }
+  );
+}
+getMovieData();
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'dataVisible' });
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "dataVisible" });
 });
 
 module.exports = router;
